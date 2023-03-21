b12ba56122e1dd7dc942f30237a63d8bb3617857
oklukaan <kaan.oklu@hotmail.com>
2023-03-21 16:11:45 +0300

    fix localhost

--- client/script.js
+++ client/script.js
@@ -160,7 +160,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://localhost:500', {
+    const response = await fetch('https://server-w6wp.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
